5d9a4ef5c95c68251a5ee77b2641aef2140461be
tiggy31 <tigist_alemu@fitnyc.edu>
2020-06-29 18:05:07 -0400

    New New

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User  
+     
+ attr_accessor :knowledge
+
+ def initialize
+      @knowledge = []
+
+ end
+
+ def learn(string_knowledge)
+
+    @knowledge << string_knowledge
+ end
 
 end
+
+# s1 = Student.new("Tiggy","Alemu")
+# U1 = User.new("hope","takes")
+# t1 = Teacher.new("Otha","fields")
---
+++ run.rb
@@ -0,0 +1,8 @@
+require "pry"
+
+
+s1 = Student.new("Tiggy","Alemu")
+u1 = User.new("hope","takes")
+t1 = Teacher.new("Otha","fields")
+
+binding.pry
